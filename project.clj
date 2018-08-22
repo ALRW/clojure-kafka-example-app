3e1290815f68c7dceeacd406404448d30fe7aeaa
ALRW <andrew.werner@fundingcircle.com>
2018-08-22 13:39:52 +0100

    [AW] udpated project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject kafka-example "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "An example app that is designed to show how to use clojure, component, kafka and spec"
+  :url "https://github.com/ALRW/clojure-kafka-example-app"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]]
