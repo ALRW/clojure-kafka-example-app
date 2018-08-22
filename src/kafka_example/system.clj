250b4488bbedcc9023a41063442ba99471b50323
ALRW <andrew.werner@fundingcircle.com>
2018-08-22 15:08:49 +0100

    [AW] added base kafka properties

---
+++ src/kafka_example/system.clj
@@ -0,0 +1,12 @@
+(ns kafka-example.system
+  (:require [com.stuartsierra.component :as component])
+  (:import org.apache.kafka.common.serialization.Serdes
+           [org.apache.kafka.streams StreamsConfig]))
+
+(def props
+  (let [properties (java.util.Properties.)]
+    (.put properties StreamsConfig/APPLICATION_ID_CONFIG "kafka-example")
+    (.put properties StreamsConfig/BOOTSTRAP_SERVERS_CONFIG "localhost:9092")
+    (.put properties StreamsConfig/DEFAULT_KEY_SERDE_CLASS_CONFIG  (.getName (.getClass (.String))))
+    (.put properties StreamsConfig/DEFAULT_VALUE_SERDE_CLASS_CONFIG  (.getName (.getClass (.String))))
+    properties))
