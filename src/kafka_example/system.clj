54b76fbadd6f424458caf7b6e34c716a7c353923
ALRW <andrew.werner@fundingcircle.com>
2018-08-23 14:22:10 +0100

    [AW] updated props to return correct config map

--- src/kafka_example/system.clj
+++ src/kafka_example/system.clj
@@ -7,6 +7,6 @@
   (let [properties (java.util.Properties.)]
     (.put properties StreamsConfig/APPLICATION_ID_CONFIG "kafka-example")
     (.put properties StreamsConfig/BOOTSTRAP_SERVERS_CONFIG "localhost:9092")
-    (.put properties StreamsConfig/DEFAULT_KEY_SERDE_CLASS_CONFIG  (.getName (.getClass (.String))))
-    (.put properties StreamsConfig/DEFAULT_VALUE_SERDE_CLASS_CONFIG  (.getName (.getClass (.String))))
+    (.put properties StreamsConfig/DEFAULT_KEY_SERDE_CLASS_CONFIG  (.getName (.getClass (Serdes/String))))
+    (.put properties StreamsConfig/DEFAULT_VALUE_SERDE_CLASS_CONFIG  (.getName (.getClass (Serdes/String))))
     properties))
