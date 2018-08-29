c05eee95e2fccb4d6bb2e21474ca3a2a8d80e0b6
ALRW <awerner1@googlemail.com>
2018-08-29 19:56:21 +0100

    [AW] removed unnecessary imports

--- src/kafka_example/core.clj
+++ src/kafka_example/core.clj
@@ -1,9 +1,6 @@
 (ns kafka-example.core
   (:require [kafka-example.system :refer [base-system]]
             [com.stuartsierra.component :as component])
-  (:import org.apache.kafka.common.serialization.Serdes
-           [org.apache.kafka.streams.kstream ValueMapper]
-           [org.apache.kafka.streams StreamsConfig KafkaStreams StreamsBuilder])
   (:gen-class))
 
 (defn -main
