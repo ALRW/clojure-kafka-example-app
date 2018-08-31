82dfa9e9fed1a2516a953157b0979c843ba03d0f
ALRW <andrew.werner@fundingcircle.com>
2018-08-31 15:33:25 +0100

    [AW] using lein ancient to upgrade outdated deps

--- project.clj
+++ project.clj
@@ -3,9 +3,9 @@
   :url "https://github.com/ALRW/clojure-kafka-example-app"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
+  :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojure/tools.logging "0.4.1"]
-                 [org.slf4j/slf4j-log4j12 "1.7.1"]
+                 [org.slf4j/slf4j-log4j12 "1.7.25"]
                  [log4j/log4j "1.2.17" :exclusions [javax.mail/mail
                                                     javax.jms/jms
                                                     com.sun.jmdk/jmxtools
